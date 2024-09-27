ac8a1044fb2bc4d2dd095047438b8eb967a7296b
23CE143 <abc@gmail.com>
2024-09-27 22:35:16 +0530

    Authentication of signup completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import Chat from './pages/Chat/Chat'
 import ProfileUpdate from './pages/ProfileUpdate/ProfileUpdate'
 import Login from './pages/login/Login'
 import { Route } from 'react-router-dom'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div>
+      <ToastContainer/>
      <Routes>
      
      <Route path='/' element={<Login/>}/>
---
+++ src/config/firebase.js
@@ -0,0 +1,42 @@
+import { initializeApp } from "firebase/app";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { getFirestore, setDoc } from "firebase/firestore";
+import { toast } from "react-toastify";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDNU-ijs6oA4t_AOz1jM-v80XU9y_hvxx8",
+  authDomain: "chat-app-gs-fd5a0.firebaseapp.com",
+  projectId: "chat-app-gs-fd5a0",
+  storageBucket: "chat-app-gs-fd5a0.appspot.com",
+  messagingSenderId: "812873589178",
+  appId: "1:812873589178:web:96d044c8be9b2f721abd83"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const signup = async (username,email,password) => {
+    try{
+        const res = await createUserWithEmailAndPassword(auth,email,password);
+        const user = res.user;
+        await setDoc(doc(db,"users",user.uid),{
+            id:user.uid,
+            username:username.toLowerCase(),
+            email,
+            name:"",
+            avatar:"",
+            bio:"Hey, There i am using chat app",
+            lastSeen:Date.now()
+        })
+        await setDoc(doc(db,"chats",user.uid),{
+            chatData:[]
+        })
+    } catch(error){
+        console.error(error)
+        toast.error(error.code)
+    }
+}
+
+export {signup}
