0cac2ac0afaaa3ace958a57fdbfb45e206646111
Arpita4 <23ce143@charusat.edu.in>
2024-10-02 16:40:34 +0530

    firestore update

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { Routes, useNavigate } from 'react-router-dom'
 //import Login from './pages/login/Login'
 //import Login from './pages/Login/Login'
@@ -10,15 +10,19 @@ import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { onAuthStateChanged } from 'firebase/auth'
 import { auth } from './config/firebase'
+import { AppContext } from './context/AppContext'
 
 const App = () => {
 
 const navigate = useNavigate();
+const {loadUserData} = useContext(AppContext)
 
 useEffect(() => {
     onAuthStateChanged(auth, async (user)=>{
         if(user){
           navigate('/chat');
+          // console.log(user);
+          await loadUserData(user.uid);
         }
         else{
           navigate('/');
